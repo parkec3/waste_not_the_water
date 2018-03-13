b923ec9c1446c5b83d4d2f9c4880ee51f82f2218
Yuening Wang <yw246@uw.edu>
2018-03-13 00:10:41 -0700

    The docstrings are added to functions

--- waste_not_the_water/tests/test_basemap.py
+++ waste_not_the_water/tests/test_basemap.py
@@ -1,3 +1,4 @@
+import waste_not_the_water
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,9 +7,10 @@ import pandas as pd
 from pandas import *
 import folium
 
-import basemap
+from waste_not_the_water import basemap
 
 def test_data_import():
+    """This function tests if the data organizing function works as expected"""
     dff =basemap.data_import_clean()
     dff.reset_index(inplace=True)
     c = dff['Capacity']
@@ -16,11 +18,13 @@ def test_data_import():
         assert c[i] <= c[i+1],"The dataframe did not sort as expected"
 
 def test_create_basic_list():
+    """The function check if all four lists are created properly"""
     dff = basemap.data_import_clean()
     lat,lon,capacity,magnitude = basemap.create_basic_list(dff)
     assert len(lat) == len(lon) == len(capacity) == len(magnitude),"List creation is not proper"
 
 def test_color_marker():
+    "The function tests if the color are assigned as expected to the data point"""
     test_list = [4,9,44,64,78,90]
     color_list=[]
     for index in test_list:
