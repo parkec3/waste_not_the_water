db500d787f1ab9d4dd17ff9d23b6c7f8124d28bd
Caitlin Parke <parkec3@uw.edu>
2018-03-13 11:08:29 -0700

    Added the correct file path for the data csv files

--- waste_not_the_water/datacleaning.py
+++ waste_not_the_water/datacleaning.py
@@ -1,5 +1,5 @@
 import pandas as pd
-data = pd.read_csv('T_UWWTPS.csv')
+data = pd.read_csv('data/T_UWWTPS.csv')
 df_toclean = pd.DataFrame(data = {'Member State': data['rptMStateKey'], 'Latitude': data['uwwLatitude'], 
 'Longitude': data['uwwLongitude'], 'LoadEntering': data['uwwLoadEnteringUWWTP'], 'Capacity': 
 data['uwwCapacity'], 'NRemoval': data['uwwNRemoval'], 'PRemoval': data['uwwPRemoval']})
