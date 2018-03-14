97c082c3fa7b376387f7be099719fb6fe2952a70
Sijia Xiao <lenaxiao@uw.edu>
2018-03-14 00:44:05 -0700

    change code style

--- waste_not_the_water/linear_regression.py
+++ waste_not_the_water/linear_regression.py
@@ -1,3 +1,5 @@
+import waste_not_the_water
+
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -10,33 +12,43 @@ import xlrd
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 
+
 def data_cleaning():
     df_cap = pd.read_csv('T_UWWTPS.csv')
-    df_cap2 = df_cap.drop(df_cap.index[4332]) # remove abnormal point
-    df_cleaned = pd.DataFrame(data = {'LoadEntering': df_cap2['uwwLoadEnteringUWWTP'],'Capacity': df_cap2['uwwCapacity'], 
-                            'NRemoval':df_cap2['uwwNRemoval'],'PRemoval':df_cap2['uwwPRemoval'],
-                            'Longitude': df_cap2['uwwLongitude'], 'Latitude':df_cap2['uwwLatitude']})
+    df_cap2 = df_cap.drop(df_cap.index[4332])  # remove abnormal point
+    df_cleaned = pd.DataFrame(
+        data={'LoadEntering': df_cap2['uwwLoadEnteringUWWTP'],
+              'Capacity': df_cap2['uwwCapacity'],
+              'NRemoval': df_cap2['uwwNRemoval'],
+              'PRemoval': df_cap2['uwwPRemoval'],
+              'Longitude': df_cap2['uwwLongitude'],
+              'Latitude': df_cap2['uwwLatitude']})
     df_no_missing = df_cleaned.dropna()
     df_no_zeros = df_no_missing[df_no_missing.LoadEntering != 0]
     df = df_no_zeros[df_no_zeros.Capacity != 0]
     return df
+
 
 # use this wrapped function for data training
 # input data as a dataframe used for training
 def linear_regression_result():
     df = data_cleaning()
     train, test = split_train_test(df)
-    filename_lr, y_predict_lr = linear_regr(train, test[['LoadEntering','Longitude','Latitude']])
+    filename_lr, y_predict_lr = linear_regr(
+        train, test[['LoadEntering', 'Longitude', 'Latitude']])
     r2_lr = r2_score(test.Capacity, y_predict_lr)
     filename_rr, y_predict_rr = ridge_regr(train, test)
     r2_rr = r2_score((test/test.std()).Capacity, y_predict_rr)
     mse_rr = mean_squared_error((test/test.std()).Capacity, y_predict_rr)
     return r2_lr, filename_lr, r2_rr, mse_rr, filename_rr
-    
+
+
 def split_train_test(df):
-    df_split = df[['Capacity','LoadEntering','Longitude','Latitude']]
-    train,test = train_test_split(df_split, test_size = 0.1, random_state=1010)
-    return train,test
+    df_split = df[['Capacity', 'LoadEntering', 'Longitude', 'Latitude']]
+    train, test = train_test_split(
+        df_split, test_size = 0.1, random_state = 1010)
+    return train, test
+
 
 def linear_regr(df_train, x_test):
     model = smf.ols("Capacity ~ LoadEntering + Longitude * Latitude", df_train)
@@ -48,28 +60,34 @@ def linear_regr(df_train, x_test):
     object_loc.close()
     return filename, y_predict
 
+
 # Ridge Regression
 def ridge_regr(train, test):
-    train_normalized=train/train.std()
-    test_normalized=test/test.std()
-    heat_ridge=Ridge()
-    a=1e0
-    heat_ridge.set_params(alpha=a)
-    heat_ridge.fit(train_normalized[['Latitude','LoadEntering','Longitude']],train_normalized.Capacity)
-    y_predict = heat_ridge.predict(test_normalized[['Latitude','LoadEntering','Longitude']])
+    train_normalized = train / train.std()
+    test_normalized = test / test.std()
+    heat_ridge = Ridge()
+    a = 1e0
+    heat_ridge.set_params(alpha = a)
+    heat_ridge.fit(
+        train_normalized[['Latitude', 'LoadEntering', 'Longitude']],
+        train_normalized.Capacity)
+    y_predict = heat_ridge.predict(
+        test_normalized[['Latitude', 'LoadEntering', 'Longitude']])
     filename = "ridge_result.sav"
     object_loc = open(filename, 'wb')
     pickle.dump(heat_ridge, object_loc)
     object_loc.close()
     return filename, y_predict
 
+
 # this function is used for interactive purpose
-# input filename for the prediction. It should be shown after you call that wrapped function
+# input filename for the prediction.
+# It should be shown after you call that wrapped function
 def customer_inter(customer, filename_lr, filename_rr):
     result = pickle.load(open(filename_lr, 'rb'))
     heat_ridge = pickle.load(open(filename_rr, 'rb'))
-    y_customer_lr = result.predict(customer[['Latitude', 'Longitude', 'LoadEntering']])
-    y_customer_rr = heat_ridge.predict(customer[['Latitude', 'LoadEntering', 'Longitude']])
+    y_customer_lr = result.predict(
+        customer[['Latitude', 'Longitude', 'LoadEntering']])
+    y_customer_rr = heat_ridge.predict(
+        customer[['Latitude', 'LoadEntering', 'Longitude']])
     return y_customer_lr, y_customer_rr
-
-
--- waste_not_the_water/tests/test_linear_regression.py
+++ waste_not_the_water/tests/test_linear_regression.py
@@ -3,57 +3,75 @@ import pandas as pd
 import numpy as np
 import os.path
 
+
 def test_data_cleaning():
     df = lr.data_cleaning()
-    if df.isnull().any().any() == True:
+    if df.isnull().any().any() is True:
         raise Exception('Bad type, df is not a DataFrame!')
-    if ((df.LoadEntering.all()) and (df.Capacity.all())) == False:
+    if ((df.LoadEntering.all()) and (df.Capacity.all())) is False:
         raise Exception('Zeros in LoadEntering and Capacity!')
+
 
 def test_linear_regression_result():
     df = pd.read_csv('clean.csv')
     df_sample = df.sample(100)
-    r2_lr, filename_lr, r2_rr, mse_rr, filename_rr = lr.linear_regression_result()
+    r2_rr, filename_lr,
+    r2_rr, mse_rr, filename_rr = lr.linear_regression_result()
     if r2_lr > 1 or r2_lr < 0 or r2_rr > 1 or r2_rr < 0:
         raise Exception('Coefficient function is wrong!')
-    
+
+
 def test_split_train_test():
     df = pd.read_csv('clean.csv')
     df_sample = df.sample(100)
     train, test = lr.split_train_test(df_sample)
-    if (train.empty == True or test.empty == True):
+    if (train.empty is True or test.empty is True):
         raise Exception('DataFrame is broken!')
     if (len(test) != 10 or len(train) != 90):
         raise Exception('Wrong train_test_split function!')
+
 
 def test_linear_regr():
     df = pd.read_csv('clean.csv')
     df_train = df.sample(80)
     x_test = df[['LoadEntering', 'Longitude', 'Latitude']].sample(20)
-    customer = pd.DataFrame(data = {'LoadEntering': [6200.0], 'Longitude': [17.0], 'Latitude': [47.0]})
+    customer = pd.DataFrame(
+        data={'LoadEntering': [6200.0],
+              'Longitude': [17.0],
+              'Latitude': [47.0]})
     filename, y_predict = lr.linear_regr(df_train, x_test)
-    if y_predict.any() == float('Inf'):
-        raise Exception('Prediction of customer cannot be infinite. Linear Regression function is wrong!')
-    assert np.isnan(y_predict).any() == False, 'Prediction function has an error!'
-    if os.path.isfile(filename) == False:
+    if y_predict.any() is float('Inf'):
+        raise Exception('Prediction of customer cannot be infinite. \
+            Linear Regression function is wrong!')
+    assert np.isnan(y_predict).any() is False,
+    'Prediction function has an error!'
+    if os.path.isfile(filename) is False:
         raise Exception('File does not exist!')
+
 
 def test_ridge_regr():
     df = pd.read_csv('clean.csv')
-    train = df[['LoadEntering', 'Latitude','Longitude', 'Capacity']].sample(80)
-    test = df[['LoadEntering', 'Latitude','Longitude', 'Capacity']].sample(20)
+    train = df[['LoadEntering', 'Latitude',
+                'Longitude', 'Capacity']].sample(80)
+    test = df[['LoadEntering', 'Latitude',
+               'Longitude', 'Capacity']].sample(20)
     filename, y_predict = lr.ridge_regr(train, test)
-    if os.path.isfile(filename) == False:
+    if os.path.isfile(filename) is False:
         raise Exception('File does not exist!')
 
+
 def test_customer_inter():
-    customer = pd.DataFrame(data = {'LoadEntering': [6200.0], 'Longitude': [17.0], 'Latitude': [47.0]})
+    customer = pd.DataFrame(
+        data={'LoadEntering': [6200.0],
+              'Longitude': [17.0],
+              'Latitude': [47.0]})
     filename_lr = "linear_result.sav"
     filename_rr = "ridge_result.sav"
-    y_customer_lr, y_customer_rr = lr.customer_inter(customer, filename_lr, filename_rr)
-    if y_customer_lr.any() == float('Inf'):
-        raise Exception('Prediction of customer cannot be infinite. Linear Regression function is wrong!')
-    if y_customer_rr.any() == float('Inf'):
-        raise Exception('Prediction of customer cannot be infinite. Ridge Regression function is wrong!')
-
-
+    y_customer_lr, y_customer_rr = lr.customer_inter(
+        customer, filename_lr, filename_rr)
+    if y_customer_lr.any() is float('Inf'):
+        raise Exception('Prediction of customer cannot be infinite. \
+            Linear Regression function is wrong!')
+    if y_customer_rr.any() is float('Inf'):
+        raise Exception('Prediction of customer cannot be infinite. \
+            Ridge Regression function is wrong!')
